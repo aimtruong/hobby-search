d1f2543344947ff3ab5213d8a71e8758b773b22c
lord vader <dillon.roth11@gmail.com>
2022-02-04 16:55:52 -0800

    search button loads results page with youtube results in console

--- assets/results.js
+++ assets/results.js
@@ -4,3 +4,26 @@ var dropdownEl = document.getElementById('dropdown')
 dropdownbtn.addEventListener('click', function() {
   dropdownEl.classList.toggle('active');
 })
+
+var getHobby = function() {
+  var queryString = document.location.search;
+    var hobby = queryString.split("=")[1];
+    if (hobby) {
+        getYoutubeResults(hobby);
+    } else {
+        document.location.replace("./index.html");
+    }
+}
+
+var getYoutubeResults = function(hobby) {
+  var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" + hobby + "&type=video&maxResults=10&key=AIzaSyCIdf8UZgAIhrjvw1jNT-u3Wk9bAAOqdxU"
+
+  fetch(apiUrl)
+    .then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      })
+    });
+};
+
+getHobby();
