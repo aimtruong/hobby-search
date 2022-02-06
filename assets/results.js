526a7030199c569997d2b26832ba941d05a7c609
Aimee Truong <aimtruong@ucdavis.edu>
2022-02-05 22:48:19 -0800

    display YT video with title

--- assets/results.js
+++ assets/results.js
@@ -17,17 +17,43 @@ var getHobby = function() {
     }
 }
 
-var getYoutubeResults = function(hobby) {
-  var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" + hobby + "&type=video&maxResults=10&key=AIzaSyCIdf8UZgAIhrjvw1jNT-u3Wk9bAAOqdxU"
 
+var getYoutubeResults = function(hobby) {
+  var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" + hobby + "-tutorial" + "&type=video&maxResults=30&key=AIzaSyCIdf8UZgAIhrjvw1jNT-u3Wk9bAAOqdxU"
+  
   fetch(apiUrl)
-    .then(function(response) {
-      response.json().then(function(data) {
-        console.log("youtube");
-        console.log(data);
-      })
-    });
+  .then(function(response) {
+    response.json().then(function(data) {
+      console.log("youtube");
+      console.log(data);
+      displayYTresults(data);
+    })
+  });
 };
+
+var ytContainer = document.querySelector(".yt-container");
+
+var ytCard = [];
+var ytTitle = [];
+var ytImg = [];
+
+var displayYTresults = function(data){
+  for(var i = 0; i < 6; i++){
+    ytCard[i] = document.createElement("div");
+      ytCard[i].classList = "yt-card col s2 card";
+        var newImage = data.items[i].snippet.thumbnails.medium.url;
+        ytCard[i].style.backgroundImage = "url(" + newImage + ")";
+
+    ytTitle[i] = document.createElement("p");
+      ytTitle[i].textContent = data.items[i].snippet.title;
+      ytTitle[i].classList.add("crdtitle");
+
+    ytContainer.appendChild(ytCard[i]);
+    ytCard[i].appendChild(ytTitle[i]);
+    }
+};
+
+
 
 var getRedditResults = function(hobby) {
   var apiUrl = "https://www.reddit.com/search.json?q=" + hobby;
