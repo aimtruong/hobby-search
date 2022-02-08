fd8b7defa444ff46e61964d9f794fe76ab8a6b23
lord vader <dillon.roth11@gmail.com>
2022-02-08 10:54:07 -0800

    paginiation code block added

--- assets/results.js
+++ assets/results.js
@@ -41,28 +41,86 @@ var ytTitle = [];
 var ytImg = [];
 var ytData = [];
 
+
 // displays YT videos on site
 var displayYTresults = function(data){
-  for(var i = 0; i <= 5 /*data.items.length*/; i++){
-    ytCard[i] = document.createElement("a");
+  var current_page = 1;
+  var results_per_page = 6;
+  
+  var youtubeResultsArr = [];
+  
+  // push youtube results to arr
+  for (var i = 0; i < data.items.length; i++) {
+    youtubeResultsArr.push(data.items[i]);
+  }
+  
+  function prevPage()
+  {
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+  }
+  
+  function nextPage()
+  { 
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+  }
+    
+  function changePage(page)
+  {
+    var btn_next = document.getElementById("next-btn");
+    var btn_prev = document.getElementById("prev-btn");
+    var container = document.querySelector(".yt-container");
+    var page_span = document.getElementById("page");
+ 
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    container.innerHTML = "";
+
+    //display youtube results on page
+    for (var i = (page-1) * results_per_page; i < (page * results_per_page); i++) {
+      ytCard[i] = document.createElement("a");
       ytCard[i].classList = "yt-card col s2 card";
-        var newImage = data.items[i].snippet.thumbnails.medium.url;
+        var newImage = youtubeResultsArr[i].snippet.thumbnails.medium.url;
         ytCard[i].style.backgroundImage = "url(" + newImage + ")";
 
-    ytTitle[i] = document.createElement("p");
-      ytTitle[i].textContent = data.items[i].snippet.title;
+      ytTitle[i] = document.createElement("p");
+      ytTitle[i].textContent = youtubeResultsArr[i].snippet.title;
       ytTitle[i].classList.add("crdtitle");
 
-      ytCard[i].href ="https://www.youtube.com/watch?v=" + data.items[i].id.videoId;
+      ytCard[i].href ="https://www.youtube.com/watch?v=" + youtubeResultsArr[i].id.videoId;
       ytCard[i].setAttribute("target", "_blank");
-      
-      ytCard[i].addEventListener("click", function(){
-    })
-      
+
       ytContainer.appendChild(ytCard[i]);
       ytCard[i].appendChild(ytTitle[i]);
+    }
+    // page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
   }
 
+  function numPages()
+  {
+    return Math.ceil(youtubeResultsArr.length / results_per_page);
+  }
+
+  changePage(1);
 };
 
 
